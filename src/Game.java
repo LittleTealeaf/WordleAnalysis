009c1d53ee2feac18b30c92bb10cc43552664493
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-06-11 16:06:36 -0400

    something's wierd going on

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,8 @@
 import java.io.File;
 import java.nio.file.Files;
+import java.util.LinkedList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 public class Game {
 
@@ -15,18 +16,66 @@ public class Game {
     }
 
     public static void main(String[] args) {
-        Game game = new Game();
-        System.out.println(game.getWord());
+        Game game = new Game(); System.out.println(game.getAnswer()); System.out.println(game.isValidWord("adieu")); System.out.println(game.isValidWord("rodeo"));
     }
 
-    private String word;
+    private final String answer;
+
+    private final List<Guess> guesses = new LinkedList<>();
 
     public Game() {
-        word = WORDS.get((int) (WORDS.size() * Math.random()));
+        answer = WORDS.get((int) (WORDS.size() * Math.random()));
     }
 
-    public String getWord() {
-        return word;
+    public String getAnswer() {
+        return answer;
     }
 
+    public void guess(String word) {
+
+        Result[] results = new Result[word.length()];
+
+        List<Character> stack = answer.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == answer.charAt(i)) {
+                results[i] = Result.CORRECT;
+            } else if (stack.contains(word.charAt(i))) {
+                results[i] = Result.PARTIAL; stack.remove((Character) word.charAt(i));
+            }
+        }
+
+        guesses.add(new Guess(word, results));
+    }
+
+    public boolean isValidWord(String word) {
+
+        return guesses.parallelStream().noneMatch(guess -> {
+
+            List<Character> stack = word.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+
+            for (int i = 0; i < guess.word().length(); i++) {
+
+                switch (guess.results()[i]) {
+                    case WRONG -> {
+                        if (stack.contains(guess.word().charAt(i))) {
+                            return true;
+                        }
+                    } case PARTIAL -> {
+                        if (guess.word().charAt(i) == word.charAt(i)) {
+                            return true;
+                        } if (!stack.contains(guess.word().charAt(i))) {
+                            return true;
+                        } stack.remove((Character) guess.word().charAt(i));
+                    } case CORRECT -> {
+                        if (word.charAt(i) != guess.word().charAt(i)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        });
+    }
 }
---
+++ src/Guess.java
@@ -0,0 +1,1 @@
+public record Guess(String word, Result[] results) {}
